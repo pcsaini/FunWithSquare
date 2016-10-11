a09d3f402f0062b79be78b12e3b26ac9e794eeb2
pcsaini <premchandsaini779@gmail.com>
2016-10-11 12:27:34 +0530

    add instructions

--- app/src/main/java/com/example/pcsaini779/hackathon/MainActivity.java
+++ app/src/main/java/com/example/pcsaini779/hackathon/MainActivity.java
@@ -8,18 +8,27 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnPlay;
+    Button btnPlay,btnInst;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         btnPlay = (Button) findViewById(R.id.btnPlay);
-
+        btnInst = (Button) findViewById(R.id.btnInst);
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this,Game.class));
+                finish();
+            }
+        });
+        btnInst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Instructions.class));
                 finish();
             }
         });
