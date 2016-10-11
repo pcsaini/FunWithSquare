04675cf141649a64dca329fd7590be018855e741
pcsaini <premchandsaini779@gmail.com>
2016-10-11 11:42:54 +0530

    game layout

---
+++ app/src/main/java/com/example/pcsaini779/hackathon/Game.java
@@ -0,0 +1,25 @@
+package com.example.pcsaini779.hackathon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Game extends AppCompatActivity {
+
+    TextView txtView;
+    Button btnNext,btnSolve;
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        txtView = (TextView) findViewById(R.id.txtView);
+        btnNext = (Button) findViewById(R.id.btnNext);
+        btnSolve = (Button) findViewById(R.id.btnSolve);
+
+
+    }
+}
--- app/src/main/java/com/example/pcsaini779/hackathon/MainActivity.java
+++ app/src/main/java/com/example/pcsaini779/hackathon/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.pcsaini779.hackathon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnPlay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPlay = (Button) findViewById(R.id.btnPlay);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Game.class));
+                finish();
+            }
+        });
+
     }
 }
