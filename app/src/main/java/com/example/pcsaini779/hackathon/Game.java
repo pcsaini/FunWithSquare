d2940dd801e4abab8579ab953ed0e3dd3983df86
pcsaini <premchandsaini779@gmail.com>
2016-10-18 23:00:05 +0530

    some change for submission

--- app/src/main/java/com/example/pcsaini779/hackathon/Game.java
+++ app/src/main/java/com/example/pcsaini779/hackathon/Game.java
@@ -15,16 +15,16 @@ import java.util.Random;
 
 public class Game extends AppCompatActivity {
 
-    TextView txtView,CorrectNum,Score,Round;
-    Boolean flag=false;
-    Button btnNext,btnSolve,OK;
+    TextView txtView, CorrectNum, Score, Round,nextRound;
+    Boolean flag = false;
+    Button btnNext, btnSolve, OK;
     EditText editText;
     Random r;
-    String Square="YES";
-    int randomNumber,size=0,a,start=3,control=0,score=0,x=0,k=0;
+    String Square = "YES";
+    int randomNumber, size = 0, a, start = 3, control = 0, score = 0, x = 0, k = 0, turn = 1, firstRoundScore = 0, secondRoundScore = 0, thirdRoundScore = 0;
     ArrayList<Integer> y;
-    PatriciaTrieTest ptt =new PatriciaTrieTest();
-    PatriciaTrie pt =new PatriciaTrie();
+    PatriciaTrieTest ptt = new PatriciaTrieTest();
+    PatriciaTrie pt = new PatriciaTrie();
 
 
     @Override
@@ -34,18 +34,80 @@ public class Game extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        r=new Random();
-        y=new ArrayList<Integer>();
+        r = new Random();
+        y = new ArrayList<Integer>();
         txtView = (TextView) findViewById(R.id.txtView);
         Round = (TextView) findViewById(R.id.round);
         Score = (TextView) findViewById(R.id.score);
         CorrectNum = (TextView) findViewById(R.id.corrNum);
         btnNext = (Button) findViewById(R.id.btnNext);
         btnSolve = (Button) findViewById(R.id.btnSolve);
-        editText=(EditText)findViewById(R.id.pickNumber);
-        OK=(Button)findViewById(R.id.btnSubmit);
+        editText = (EditText) findViewById(R.id.pickNumber);
+        OK = (Button) findViewById(R.id.btnSubmit);
+        nextRound = (TextView) findViewById(R.id.nextRound);
 
 
+        pickNumber(3);
+
+        btnSolve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flag = true;
+                //txtView.setText(""+randomNumber);
+                CorrectNum.setText(Math.sqrt(randomNumber) + " : " + randomNumber);
+                OK.setEnabled(false);
+                btnSolve.setEnabled(false);
+
+            }
+        });
+        OK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setRound();
+            }
+        });
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OK.setEnabled(true);
+                if (flag == true) {
+                    if (control < 4) {
+                        control++;
+                        turn++;
+                        Round.setText("Round " + (start - 2) +" : Turn " + turn);
+                        flag = false;
+                        pickNumber(3);
+                        CorrectNum.setText("_");
+
+                    } else {
+                        if (score > 10) {
+                            nextRound.setVisibility(View.VISIBLE);
+                            nextRound.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    nextRound();
+                                    nextRound.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        } else {
+                            Toast.makeText(Game.this,"Your Score is Low Please Replay",Toast.LENGTH_LONG).show();
+                            nextRound.setVisibility(View.VISIBLE);
+                            nextRound.setText("Replay");
+                            nextRound.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    replayRound();
+                                    nextRound.setVisibility(View.INVISIBLE);
+                                }
+                            });
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    public void pickNumber(int start) {
         y = ptt.game(pt, start, Square);
         size = y.size();
         k = size;
@@ -55,109 +117,61 @@ public class Game extends AppCompatActivity {
         //size = size - 1;
         a = randomNumber / 10;
         txtView.setText("" + a);
+    }
 
-
-
-        btnSolve.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                flag = true;
-                //txtView.setText(""+randomNumber);
-                CorrectNum.setText(Math.sqrt(randomNumber)+" : "+randomNumber);
-
+    public void setRound() {
+        String s = editText.getText().toString();
+        if (s != "") {
+            String s1 = randomNumber + "";
+            String s2 = a + "";
+            String s3 = s2 + s;
+            flag = true;
+            if (s1.equals(s3)) {
+                txtView.setText("" + s3);
+                score += 5;
+                Toast.makeText(Game.this, "You Won Press Next", Toast.LENGTH_LONG).show();
+            } else {
+                txtView.setText("" + s3);
+                Toast.makeText(Game.this, "You Loss Press Next", Toast.LENGTH_LONG).show();
+                score -= 2;
             }
-        });
-        OK.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String s=editText.getText().toString();
-                if(s!=""){
-                    String s1=randomNumber+"";
-                    String s2=a+"";
-                    String s3=s2+s;
-                    flag=true;
-                    if(s1.equals(s3)){
-                        txtView.setText(""+s3);
-                        score+=5;
-                        Score.setText(""+score);
+            CorrectNum.setText(Math.sqrt(randomNumber) + " : " + randomNumber);
+            editText.setText("");
+            Score.setText("" + score);
+        } else {
+            flag = false;
+        }
+        OK.setEnabled(false);
+        if(start == 3){
+            firstRoundScore +=score;
+        }
+        else if (start == 4){
+            secondRoundScore +=score;
+        }
+        else if(start == 5){
+            thirdRoundScore +=score;
+        }
+    }
+    public void nextRound() {
+        Round.setText("Round " + (start - 1));
+        pt.makeEmpty();
+        start++;
+        pickNumber(3);
+        control = 0;
+        //score = 0;
+        turn = 0;
+        CorrectNum.setText("0");
+    }
 
-                        //Toast.makeText(this,"You Won Press Next",Toast.LENGTH_LONG).show();
-                    }
-                    else {
-                        txtView.setText(""+s3);
-                        if(score>2){
-                            score-=2;
-                        }}
-                    CorrectNum.setText(Math.sqrt(randomNumber)+" : "+randomNumber);
-                    editText.setText("");
-                    Score.setText(""+score);
-                }
-                else
-                {
-                    flag=false;
-                }
-
-            }
-        });
-        btnNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               /* if (score > 50) {
-                    //txtView.setText("Your are WINNER and  You will Succeed in your life");
-                    btnNext.setEnabled(false);
-
-                }*/
-                if (flag == true) {
-                    if (control < 5) {
-                        control++;
-                        flag = false;
-                        int x = r.nextInt(y.size());
-                        randomNumber = y.get(x);
-                        y.remove(x);
-                        size = size - 1;
-                        a = randomNumber / 10;
-                        txtView.setText("" + a);
-                        CorrectNum.setText("0");
-
-
-                    } else {
-                        if (score > 10) {
-                            Round.setText("Round "+(start-1));
-                            pt.makeEmpty();
-                            start++;
-                            y = ptt.game(pt, start, Square);
-                            size = y.size();
-                            k = size;
-                            x = r.nextInt(y.size());
-                            randomNumber = y.get(x);
-
-                            y.remove(x);
-                            //size = size - 1;
-                            a = randomNumber / 10;
-                            txtView.setText("" + a);
-                            control = 0;
-                            score = 0;
-                            CorrectNum.setText("0");
-                        } else {
-                            pt.makeEmpty();
-
-                            y = ptt.game(pt, start, Square);
-                            size = y.size();
-                            k = size;
-                            x = r.nextInt(y.size());
-                            randomNumber = y.get(x);
-                            y.remove(x);
-                            //size = size - 1;
-                            a = randomNumber / 10;
-                            txtView.setText("" + a);
-                            control = 0;
-                            score = 0;
-
-
-                        }
-                    }
-                }
-            }
-        });
+    public void replayRound() {
+        pt.makeEmpty();
+        control = 0;
+        turn = 1;
+        score = 0;
+        pickNumber(3);
+    }
+    public void firstRound(){
+        pt.makeEmpty();
+        pickNumber(3);
     }
 }
--- app/src/main/java/com/example/pcsaini779/hackathon/MainActivity.java
+++ app/src/main/java/com/example/pcsaini779/hackathon/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.pcsaini779.hackathon;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,Team.class));
             }
         });
+
 
     }
 }
